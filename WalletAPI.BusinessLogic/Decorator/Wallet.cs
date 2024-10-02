3901946c9ec83b841d8903301f5089fbdce34e44
nataliiarastoropova <rastoropova.nataliia@gmail.com>
2024-10-02 23:09:26 +0300

    Added decorator

---
+++ WalletAPI.BusinessLogic/Decorator/Wallet.cs
@@ -0,0 +1,9 @@
+namespace WalletAPI.BusinessLogic.Decorator;
+
+public class Wallet : IWallet
+{
+    public void MakePayment(decimal amount)
+    {
+        //Payment of {amount} processed from Basic Wallet
+    }
+}
